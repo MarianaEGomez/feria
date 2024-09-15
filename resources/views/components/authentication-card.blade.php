a283b8d0c5b87c7f1365de6ff72acf8bdf115008
MarianaEGomez <marianaegomezjara.com>
2024-09-15 10:08:47 -0300

    mejoras en el login

--- resources/views/components/authentication-card.blade.php
+++ resources/views/components/authentication-card.blade.php
@@ -1,14 +1,19 @@
 <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 background-custom">
-    <div>
-        {{ $logo }}
-    </div>
+
 
     <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+        <div>
+            {{ $logo }}
+        </div>   
         {{ $slot }}
     </div>
 </div>
 <style>
-    .background-custom{
-        background: #16697a !important;
+    .background-custom {
+        background-image: url('/fondo_foto/costanera.jpeg') !important;
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
     }
 </style>
+
